37b3bb2d82034a806b3ea12b378816875ae151b3
Gordon Charlton <75318677+GordonCharlton@users.noreply.github.com>
2024-11-17 12:23:47 +0000

    rewrote char->n

--- quackery.py
+++ quackery.py
@@ -1065,11 +1065,8 @@ protect sort.test
 
 [ sortwith $> ]               is sort$        (       [ --> [       )
 
-[ upper 47 - 0 44 clamp
-  [ table
-    -1  0  1  2  3  4  5  6  7  8  9 -1 -1 -1 -1
-    -1 -1 -1 10 11 12 13 14 15 16 17 18 19 20 21
-    22 23 24 25 26 27 28 29 30 31 32 33 34 35 -1 ]
+[ upper 48 -
+  dup 9 > if [ 7 - ]
   dup 0 base share
   within not if [ drop -1 ] ] is char->n      (       c --> n       )
 
